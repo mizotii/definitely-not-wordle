d351d2d223056b74b44feb6938cf99e11270fd7a
mizotii <mztiiiii0@gmail.com>
2026-05-02 15:56:50 -0700

    configure dev proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_BASE_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+    load: ({ mode }) => {
+      loadEnv(mode, process.cwd(), '')
+    }
+  })
+}
+
