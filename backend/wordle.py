e2fd09a688688ba068de6346b5166ece420883dd
mizotii <mztiiiii0@gmail.com>
2026-05-02 21:15:21 -0700

    fix stuff

--- backend/wordle.py
+++ backend/wordle.py
@@ -1,16 +1,9 @@
 import os
 import random
-from enum import Enum
 from typing import List, Set
 
 WORD_LENGTH = 5
 WORD_LIST_FILEPATH = os.environ.get('WORD_LIST_FILEPATH', './words.txt')
-
-class GameStatus(Enum):
-    NOT_STARTED = 0
-    IN_PROGRESS = 1
-    WON = 2
-    LOST = 3
 
 class WordList:
     def __init__(self):
@@ -23,10 +16,10 @@ class WordList:
         return guess in self.words_set
 
     def replace_current_answer(self):
-        self.current_answer = random.choice(self.words_list)
+        return random.choice(self.words_list)
 
     def _load_words(self):
-        with open(self.words_filepath, 'r') as f:
+        with open(WORD_LIST_FILEPATH, 'r') as f:
             lines = f.readlines()
             for line in lines:
                 word = line.strip().upper()
