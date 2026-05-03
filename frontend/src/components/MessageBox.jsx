85686751352c0a91791b2cfd424b9a37b2c5f7b7
mizotii <mztiiiii0@gmail.com>
2026-05-03 16:56:35 -0700

    message box

---
+++ frontend/src/components/MessageBox.jsx
@@ -0,0 +1,10 @@
+const MessageBox = ({ message }) => {
+
+    return (
+        <div className='message-box'>
+            <h2>{message}</h2>
+        </div>   
+    )
+}
+
+export default MessageBox;
