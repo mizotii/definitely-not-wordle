5d7c433263ef689a6f3c3989d9fd55c98b3a3ef0
mizotii <mztiiiii0@gmail.com>
2026-05-02 20:45:07 -0700

    factor player into helper functions (guess_word)

--- backend/helpers.py
+++ backend/helpers.py
@@ -1,4 +1,42 @@
 from collections import Counter
+from enum import Enum
+from typing import List
+
+WORD_LENGTH = 5
+
+class Color(Enum):
+    NONE = 0
+    GRAY = 1
+    YELLOW = 2
+    GREEN = 3
 
 def calc_char_freq_diff(guess, correct):
     return Counter(guess) - Counter(correct)
+
+def guess_word(guess: str, answer: str) -> List[Color]:
+    res = [Color.NONE] * WORD_LENGTH
+    char_freq_count = Counter(answer)
+
+    # first pass (green)
+    for idx, (guess_c, cur_c)in enumerate(zip(guess, answer)):
+
+        if guess_c == cur_c:
+            res[idx] = Color.GREEN
+            char_freq_count[cur_c] -= 1
+
+    # second pass (yellow)
+    for idx, guess_c in enumerate(guess):
+        if res[idx] == Color.GREEN:
+            continue
+
+        if char_freq_count[guess_c] > 0:
+            res[idx] = Color.YELLOW
+            char_freq_count[cur_c] -= 1
+
+        else:
+            res[idx] = Color.GRAY
+
+    return res
+
+def is_correct(result):
+    return result == [Color.GREEN] * 5
