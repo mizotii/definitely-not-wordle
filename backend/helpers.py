b79cf7f649ce731014efabf260d34c5c6a57a662
mizotii <mztiiiii0@gmail.com>
2026-05-02 21:13:48 -0700

    fix sanitize_guess, ditch color enum

--- backend/helpers.py
+++ backend/helpers.py
@@ -1,42 +1,41 @@
 from collections import Counter
-from enum import Enum
 from typing import List
 
 WORD_LENGTH = 5
 
-class Color(Enum):
-    NONE = 0
-    GRAY = 1
-    YELLOW = 2
-    GREEN = 3
-
 def calc_char_freq_diff(guess, correct):
     return Counter(guess) - Counter(correct)
 
-def guess_word(guess: str, answer: str) -> List[Color]:
-    res = [Color.NONE] * WORD_LENGTH
+def guess_word(guess: str, answer: str) -> List[str]:
+    res = ['none'] * WORD_LENGTH
     char_freq_count = Counter(answer)
 
     # first pass (green)
     for idx, (guess_c, cur_c)in enumerate(zip(guess, answer)):
 
         if guess_c == cur_c:
-            res[idx] = Color.GREEN
+            res[idx] = 'green'
             char_freq_count[cur_c] -= 1
 
     # second pass (yellow)
     for idx, guess_c in enumerate(guess):
-        if res[idx] == Color.GREEN:
+        if res[idx] == 'green':
             continue
 
         if char_freq_count[guess_c] > 0:
-            res[idx] = Color.YELLOW
-            char_freq_count[cur_c] -= 1
+            res[idx] = 'yellow'
+            char_freq_count[guess_c] -= 1
 
         else:
-            res[idx] = Color.GRAY
+            res[idx] = 'gray'
 
     return res
 
 def is_correct(result):
-    return result == [Color.GREEN] * 5
+    return result == ['green'] * 5
+
+def sanitize_guess(guess):
+    if not (type(guess) == str and guess.isalpha() and len(guess) == WORD_LENGTH):
+        return None
+    
+    return guess.upper().strip()
